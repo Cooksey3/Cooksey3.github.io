61b4f2b82461f8e843691799ca120a0ae30fdd01
cooksey3 <chris@cookseys.org>
2018-03-10 19:05:08 -0500

    Touched up menu. Working on image carousel

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,17 @@
-const displayMenu = (ul) => {
-  ul.style.display = 'block';
-}
+const modal = document.querySelector(".about-me");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
+
+  function toggleModal() {
+      modal.classList.toggle("show-modal");
+  }
+
+  function windowOnClick(event) {
+      if (event.target === modal) {
+          toggleModal();
+      }
+  }
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
