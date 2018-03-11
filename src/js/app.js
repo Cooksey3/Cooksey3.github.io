2e6ea038488414b4d70b3a0bc714475f0d97480f
cooksey3 <chris@cookseys.org>
2018-03-10 19:37:37 -0500

    Working on carousel

--- src/js/app.js
+++ src/js/app.js
@@ -15,3 +15,33 @@ const closeButton = document.querySelector(".close-button");
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("slider1-image");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+var slide2Index = 0;
+carousel2();
+
+function carousel2() {
+    var i;
+    var x = document.getElementsByClassName("slider2-image");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slide2Index++;
+    if (slide2Index > x.length) {slide2Index = 1} 
+    x[slide2Index-1].style.display = "block"; 
+    setTimeout(carousel2, 2000); // Change image every 2 seconds
+}
