4e13b54e4e2ffa227c267ab14afdcec0d94af91e
cooksey3 <chris@cookseys.org>
2018-03-12 09:45:07 -0400

    Broke about me

--- src/js/description.js
+++ src/js/description.js
@@ -12,3 +12,18 @@ function expandButton(event) {
 }
 
 document.addEventListener('click', expandButton, true);
+
+function expandMenu(event) {
+  if (event.target.className === "menu") {
+
+    const element = event.target.nextElementSibling;
+
+    if (element.style.display === "block") {
+      element.style.display = "none";
+    } else {
+      element.style.display = "block";
+    };
+  };
+}
+
+document.addEventListener('hover', expandMenu, true);
